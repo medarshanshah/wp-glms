913ed6455453001cba48310eb732593ef43ee379
Darshan Shah <darshanshah.ds2017@gmail.com>
2025-01-19 19:21:47 +0545

    feat: lint

--- src/components/forms/create-group/payment-form.tsx
+++ src/components/forms/create-group/payment-form.tsx
@@ -1,17 +1,15 @@
 "use client"
 
-
 type Props = {
-  userId: string
-  affiliate: boolean
-  stripeId?: string
+    userId: string
+    affiliate: boolean
+    stripeId?: string
 }
 
 // TODO: Connect the use Payments hook
 
 const PaymentForm = ({ affiliate, userId, stripeId }: Props) => {
-
-  return <div>PaymentForm</div>
+    return <div>PaymentForm</div>
 }
 
 export default PaymentForm
--- src/components/global/stripe/elements.tsx
+++ src/components/global/stripe/elements.tsx
@@ -6,6 +6,6 @@ type StripeElementsProps = {
     children: React.ReactNode
 }
 
-export const StripeElements = ({children}: StripeElementsProps) => {
+export const StripeElements = ({ children }: StripeElementsProps) => {
     return <div>{children}</div>
 }
