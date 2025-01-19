038a0e16d8e6993b7be77f83bfa2fe1fe7f0d25c
Darshan Shah <darshanshah.ds2017@gmail.com>
2025-01-19 19:20:32 +0545

    feat: added route /group/create

---
+++ src/components/global/stripe/elements.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+// TODO: add Stripe Elements
+
+type StripeElementsProps = {
+    children: React.ReactNode
+}
+
+export const StripeElements = ({children}: StripeElementsProps) => {
+    return <div>{children}</div>
+}
